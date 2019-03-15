97bd7a0f166a9130a8202e1884d8cada5a48a6be
Francisco Costa <francisco@fcosta.pt>
2019-03-15 14:51:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
+  binding.pry
   array.each { |word| if cheese_types.each {|cheese| cheese == word} then return word else nil end }
 end
