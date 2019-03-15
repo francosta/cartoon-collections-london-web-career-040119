ab596bd6cd926c7b382e950bea138c578100a610
Francisco Costa <francisco@fcosta.pt>
2019-03-15 14:35:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect {|word| puts "#{word.upcase}!"}
+  new_array = []
+  new_array << array.collect {|word| puts "#{word.upcase}!"}
 end
 
 def long_planeteer_calls# code an argument here
