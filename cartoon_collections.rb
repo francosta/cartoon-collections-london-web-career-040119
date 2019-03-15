17bbde4bf30305a1e86315b2f1ad57d292807bf3
Francisco Costa <francisco@fcosta.pt>
2019-03-15 14:28:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)
   array.each_with_index {|name, index| puts "#{index + 1} #{name}/n"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
 end
 
